311d528ac22e1cd17f70d01a6f5ce1735d1dec79
Ace-codes-swift <159598967+Ace-codes-swift@users.noreply.github.com>
2026-04-19 19:38:21 -0500

    updated project specific files to have the copyright notice at the top

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+/*Copyright © 2026  Ace Jones/ATech
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see https://www.gnu.org/licenses/. */
+
+
+
+
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
